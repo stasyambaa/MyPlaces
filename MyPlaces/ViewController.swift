e30f42b59f62d0c029797cff041d64e23ec36f9d
stasyamba <stas.shapetko93@gmail.com>
2022-02-12 23:10:39 +0200

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Станислав Шапетько on 12.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
