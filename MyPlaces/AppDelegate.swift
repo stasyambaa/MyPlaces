03454c5e3c49947e707e0e877537e4052f76a531
stasyamba <stas.shapetko93@gmail.com>
2022-02-16 01:03:50 +0200

    картинки в алерт контроллере добавление картинки с камеры в imageView

--- MyPlaces/AppDelegate.swift
+++ MyPlaces/AppDelegate.swift
@@ -6,8 +6,8 @@
 //
 
 import UIKit
-
-  @main
+ 
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class NewPlaceViewController: UITableViewController {
 
+    @IBOutlet weak var imageOfPlace: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,6 +19,8 @@ class NewPlaceViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+            let cameraIcon = UIImage(named: "camera")
+            let photoIcon = UIImage(named: "photo")
             // создаем сам контроллер
             let actionSheet = UIAlertController(title: nil,
                                                 message: nil,
@@ -26,10 +29,14 @@ class NewPlaceViewController: UITableViewController {
             let camera = UIAlertAction(title: "Camera", style: .default) { _ in
                 self.chooseImagePicker(source: .camera)
             }
+            camera.setValue(cameraIcon, forKey: "image")
+            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
             // создаем кнопку "фото"
             let photo = UIAlertAction(title: "Photo", style: .default) { _ in
                 self.chooseImagePicker(source: .photoLibrary)
             }
+            photo.setValue(photoIcon, forKey: "image")
+            photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
             // создаем кнопку "отмена"
             let cancel = UIAlertAction(title: "Cancel", style: .cancel)
                 // добавляем кнопки в контроллер
@@ -53,16 +60,27 @@ class NewPlaceViewController: UITableViewController {
         }
     }
 // MARK: word with image
-extension NewPlaceViewController {
+extension NewPlaceViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func chooseImagePicker(source: UIImagePickerController.SourceType) {
        // проверка доступности метода перед добавлением
         if
              UIImagePickerController.isSourceTypeAvailable(source) {
             let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
             // разрешаем изменение изображения, например масштабирование перед добавлением
             imagePicker.allowsEditing = true
             imagePicker.sourceType = source
             present(imagePicker, animated: true)
         }
+    }
+    // работа с полученным отредактированным из камеры или из галереи изображение
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // присваивание значения аутлету, editedImage - отредактированное изображение
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        //масштабирование под размер
+        imageOfPlace.contentMode = .scaleAspectFill
+        //обрезка по границе imageView
+        imageOfPlace.clipsToBounds = true
+        dismiss(animated: true)
     }
 }
